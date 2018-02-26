702c5b417a1e0af0029581d51d89b015a9d30989
ghumman3131 <charanghumman@gmail.com>
2018-02-26 13:37:37 +0530

    filter added

--- app/src/main/java/com/inception/emojifier/MainActivity.java
+++ app/src/main/java/com/inception/emojifier/MainActivity.java
@@ -2,7 +2,16 @@ package com.inception.emojifier;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,11 +27,16 @@ import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 import com.google.android.gms.vision.face.Landmark;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity  {
 
 
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +50,13 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        File photo = new File(Environment.getExternalStorageDirectory(), "emojify_image");
 
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,  Uri.fromFile(photo));
+
+        imageUri = Uri.fromFile(photo);
+
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
 
     }
 
@@ -45,11 +64,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
 
-            // get image from camera and store in bitmap object
+            /*Bundle extras = data.getExtras();
 
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //get image from camera and store in bitmap object
+
+           Bitmap imageBitmap = (Bitmap) extras.get("data");*/
 
 
             Button go_btn = findViewById(R.id.go_btn);
@@ -82,9 +102,24 @@ public class MainActivity extends AppCompatActivity {
 
             clicked_image.setVisibility(View.VISIBLE);
 
-            clicked_image.setImageBitmap(imageBitmap);
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
 
-            detect_faces(imageBitmap);
+                clicked_image.setImageBitmap(bitmap);
+
+                // creating FaceDetector object
+
+                detect_faces(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+           /*clicked_image.setImageBitmap(imageBitmap);
+
+           detect_faces(imageBitmap);*/
+
+
+
 
         }
     }
@@ -139,9 +174,6 @@ public class MainActivity extends AppCompatActivity {
     public  void detect_faces(Bitmap bmp)
     {
 
-
-        // creating FaceDetector object
-
         FaceDetector detector = new FaceDetector.Builder(MainActivity.this)
                 .setTrackingEnabled(false)
                 .setLandmarkType(FaceDetector.ALL_LANDMARKS)
@@ -157,8 +189,79 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(MainActivity.this , "number of faces "+faces.size() , Toast.LENGTH_SHORT).show();
 
-        // release detector
 
+
+        // creating paint object
+        Paint rectPaint = new Paint();
+        rectPaint.setStrokeWidth(5);
+        rectPaint.setColor(Color.RED);
+        rectPaint.setStyle(Paint.Style.STROKE);
+
+
+        Bitmap temporaryBitmap = Bitmap.createBitmap(bmp.getWidth(), bmp
+                .getHeight(), Bitmap.Config.RGB_565);
+
+        Canvas canvas = new Canvas(temporaryBitmap);
+        canvas.drawBitmap(bmp, 0, 0, null);
+
+
+        // iterating over each face
+
+        for (int i = 0; i < faces.size(); i++)
+        {
+            Face face = faces.valueAt(i);
+
+            float left = face.getPosition().x;
+            float top = face.getPosition().y;
+            float right = left + face.getWidth();
+            float bottom = top + face.getHeight();
+            float cornerRadius = 2.0f;
+
+            RectF rectF = new RectF(left, top, right, bottom);
+
+            canvas.drawRoundRect(rectF, cornerRadius, cornerRadius, rectPaint);
+
+            // iterating over each landmark in face
+            for (Landmark landmark : face.getLandmarks()) {
+
+                int cx = (int) (landmark.getPosition().x);
+                int cy = (int) (landmark.getPosition().y);
+
+                // canvas.drawCircle(cx, cy, 10, rectPaint);
+
+                String type = String.valueOf(landmark.getType());
+                rectPaint.setTextSize(50);
+                canvas.drawText(type, cx, cy, rectPaint);
+
+                // converting drawable left eye resource file to bitmap
+               Bitmap left_eye_bmp = BitmapFactory.decodeResource(getResources() , R.drawable.left_eye);
+
+
+               // converting drawable right eye resource file to bitmap
+               Bitmap right_eye_bmp = BitmapFactory.decodeResource(getResources() , R.drawable.right_eye);
+
+               // setting eye image on landmark location 4 which is left eye
+                if (landmark.getType() == 4) {
+                    canvas.drawBitmap(left_eye_bmp, cx , cy , null);
+                }
+
+                // setting eye image on landmark location 10 which is right eye
+                if (landmark.getType() == 10) {
+                    canvas.drawBitmap(right_eye_bmp, cx - 270, cy - 250, null);
+                }
+
+
+            }
+        }
+
+        // find view by id for imageview
+        ImageView img = findViewById(R.id.image_);
+
+        // setting bitmap mask on imageview
+        img.setImageDrawable(new BitmapDrawable(getResources(), temporaryBitmap));
+
+
+        // release face detector object
         detector.release();
 
 
